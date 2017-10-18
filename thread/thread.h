43dd10b5b053abd80caec28646e3dd19604ed1ef
pregene <pregene@gmail.com>
2017-10-18 14:51:43 +0900

    add thread

---
+++ thread/thread.h
@@ -0,0 +1,12 @@
+#if !defined(__PREGENE_COMMON_THREAD_HEADER__)
+#define __PREGENE_COMMON_THREAD_HEADER__
+
+#ifdef _WIN32
+	// windows platform
+#elif defined(__linux__)
+	// linux platform
+#else
+	// unkown platform
+#endif
+
+#endif // __PREGENE_COMMON_THREAD_HEADER__
