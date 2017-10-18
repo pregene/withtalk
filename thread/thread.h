aebfc24c7695130f5074a75dea46da28784344b4
pregene <pregene@gmail.com>
2017-10-18 18:03:23 +0900

    add pragma

--- thread/thread.h
+++ thread/thread.h
@@ -22,35 +22,51 @@
 	typedef HANDLE cond_t;
 	typedef HANDLE thread_t;
 
-	#define mutex_lock(m)
-	#define mutex_unlock(m)
-	#define mutex_init(m)
-	#define mutex_destroy(m)
-	#define cond_init(c)
-	#define cond_destroy(c)
-	#define cond_wait(c)
-	#define cond_timedwait(c, m, ms)
-	#define thread_create(h,f,p,d)
-	#define thread_term(h)
-	#define thread_termwait(h,s)
+	#define mutex_lock(m)           WaitForSingleObject(m, -1)
+    #define mutex_unlock(m)         ReleaseMutex(m)
+    #define mutex_init(m)           m=CreateMutex(0, FALSE, 0)
+    #define mutex_destroy(m)        CloseHandle(m)
+    #define cond_init(c)            c=CreateEvent(0, 0, FALSE, 0)
+    #define cond_destroy(c)         CloseHandle(c)
+    #define cond_wait(c)            ResetEvent(c);WaitForSingleObject(c, -1)
+    #define cond_timedwait(c,m,ms,r) ResetEvent(c);r=WaitForSingleObject(c, ms)
+    #define cond_signal(c)          SetEvent(c)
+    #define thread_create(h,f,p,d)  h=CreateThread(0, 1024, f, p, 0, &(d))
+    #define thread_term(h)          TerminateThread(h, 0)
+    #define thread_termwait(h, s)   do { \
+                                        if (GetExitCodeThread(h, &(s)) == FALSE) \
+                                            break; \
+                                        Sleep(100); \
+                                    } while (s==STILL_ACTIVE)
 #elif defined(__linux__)
 	// linux platform
 	#include <pthread.h>
+    #include <time.h>
+    #include <sys/time.h>
+
 	typedef pthread_mutex_t mutex_t;
 	typedef pthread_cond_t  cond_t;
 	typedef pthread_t       thread_t;
 
-	#define mutex_lock(m)
-	#define mutex_unlock(m)
-	#define mutex_init(m)
-	#define mutex_destroy(m)
-	#define cond_init(c)
-	#define cond_destroy(c)
-	#define cond_wiat(c)
-	#define cond_timedwait(c, m, ms)
-	#define thread_create(h, f, p)
-	#define thread_tern(h)
-	#define thread_termwait(h, s)
+	#define mutex_lock(m)           pthread_mutex_lock(&(m))
+    #define mutex_unlock(m)         pthread_mutex_unlock(&(m))
+    #define mutex_init(m)           pthread_mutex_init(&(m), 0)
+    #define mutex_destroy(m)        pthread_mutex_destroy(&(m))
+    #define cond_init(c)            pthread_cond_init(&(c), 0)
+    #define cond_destroy(c)         pthread_cond_destroy(&(c))
+    #define cond_signal(c)          pthread_cond_signal(&(c))
+    #define cond_wait(c, m)         pthread_cond_wait(&(c), &(m))
+    #define cond_timedwait(c,m,ms)  { \
+                                      struct timespec tv; \
+                                      struct timeval now; \
+                                      gettimeofday(&now, 0); \
+                                      tv.tv_sec = now.tv_sec + (ms/1000); \
+                                      tv.tv_nsec = (now.tv_usec + (ms % 1000)) * 1000; \
+                                      ptrhead_cond_timedwait(&(c), &(m), &tv); \
+                                    }
+    #define thread_create(h,f,p)    pthread_create(&(h), 0, f, p)
+    #define thread_term(h)          pthread_cancel(h)
+    #define thread_termwait(h, s)   pthread_join(h, (void**)&(s))
 #else
 	// unkown platform
 #endif
