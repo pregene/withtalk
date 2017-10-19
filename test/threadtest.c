d7e04e1087655d897ea21b62695a0065abba6316
pregene <pregene@gmail.com>
2017-10-19 11:41:48 +0900

    modify thread.h

--- test/threadtest.c
+++ test/threadtest.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include "thread/thread.h"
 
+PRRET PRTHAPI subthread1(PRPARAM param)
+{
+	printf("thread1\n");
+	return PRRETURN(param);
+}
+
+PRRET PRTHAPI subthread2(PRPARAM param)
+{
+	printf("thread2\n");
+	return PRRETURN(param);
+}
+
 int main(int argc, char* argv[])
 {
+	int status;
+	thread_t 	sub1_th;
+	thread_t 	sub2_th;
+	mutex_t 	sync_mutex;
+
+	thread_create(sub1_th, subthread1, NULL);
+	thread_create(sub2_th, subthread2, NULL);
+
+	thread_termwait(sub1_th, status);
+	thread_termwait(sub2_th, status);
 	return 0;
 }
--- thread/thread.h
+++ thread/thread.h
@@ -17,10 +17,16 @@
 
 #ifdef _WIN32
 	// windows platform
+	#warning "thread api on mingw gcc"
 	#include <windows.h>
 	typedef HANDLE mutex_t;
 	typedef HANDLE cond_t;
 	typedef HANDLE thread_t;
+
+	#define PRTHAPI 		WINAPI
+	typedef DWORD 	PRRET;
+	typedef LPARAM  PRPARAM;
+	#define PRRETURN(a)  0
 
 	#define mutex_lock(m)           WaitForSingleObject(m, -1)
     #define mutex_unlock(m)         ReleaseMutex(m)
@@ -31,15 +37,16 @@
     #define cond_wait(c)            ResetEvent(c);WaitForSingleObject(c, -1)
     #define cond_timedwait(c,m,ms,r) ResetEvent(c);r=WaitForSingleObject(c, ms)
     #define cond_signal(c)          SetEvent(c)
-    #define thread_create(h,f,p,d)  h=CreateThread(0, 1024, f, p, 0, &(d))
+    #define thread_create(h,f,p)  	{ DWORD d; h=CreateThread(0, 1024, f, p, 0, &d); }
     #define thread_term(h)          TerminateThread(h, 0)
     #define thread_termwait(h, s)   do { \
                                         if (GetExitCodeThread(h, &(s)) == FALSE) \
                                             break; \
                                         Sleep(100); \
                                     } while (s==STILL_ACTIVE)
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__APPLE__) && defined(__MACH__)
 	// linux platform
+    #warning "thread api on linux gcc"
 	#include <pthread.h>
     #include <time.h>
     #include <sys/time.h>
@@ -47,6 +54,11 @@
 	typedef pthread_mutex_t mutex_t;
 	typedef pthread_cond_t  cond_t;
 	typedef pthread_t       thread_t;
+
+	#define PRTHAPI 
+	typedef void* PRRET;
+	typedef void* PRPARAM;
+	#define PRRETURN(a) (PRRET) a
 
 	#define mutex_lock(m)           pthread_mutex_lock(&(m))
     #define mutex_unlock(m)         pthread_mutex_unlock(&(m))
@@ -69,6 +81,7 @@
     #define thread_termwait(h, s)   pthread_join(h, (void**)&(s))
 #else
 	// unkown platform
+    #error "Unknown Platform"
 #endif
 
 #endif // __PREGENE_COMMON_THREAD_HEADER__
